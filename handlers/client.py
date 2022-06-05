aab4a1559f41185a8d5a4d1b45ddf8772f26d815
Botir-01 <xasanovich01@icloud>
2022-06-05 21:56:33 +0500

    changed service

--- handlers/client.py
+++ handlers/client.py
@@ -401,8 +401,8 @@ async def finish(message: types.Message, state: FSMContext):
             else:
                 files = file_name.split("=+=")
         #session = smtplib.SMTP('smtp.gmail.com', 587)
-        session = smtplib.SMTP('mail.minsport.uz', 25, 'daac')
-        session.starttls()
+        session = smtplib.SMTP('mail.minsport.uz', 25, 'minsport')
+        #session.starttls()
         session.login(sender_address, sender_pass)
         text = message.as_string()
         session.sendmail(sender_address, receiver_address, text)
